64b0548bcb4df55ab7196ee857cb94451f2aa200
Evelyn Fernandes <evelyn.af22@gmail.com>
2023-11-12 18:06:19 -0300

    root

---
+++ target/classes/bd/migration/V003__teste.sql
@@ -0,0 +1,6 @@
+create table teste(
+                        id bigint not null auto_increment,
+                        nome varchar(60) not null
+                        
+                        primary key (id)
+);
